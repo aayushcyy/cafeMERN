e927e3d4f86bdb2c279b24c0a67228518afee097
Aayush Chaudhary <aayushcyy@gmail.com>
2025-03-14 13:07:39 +0530

    changed the design of navbar

--- frontend/app/Component/Navbar.jsx
+++ frontend/app/Component/Navbar.jsx
@@ -9,45 +9,55 @@ import {
 
 export default function MyNavbar() {
   return (
-    <Navbar
-      shouldHideOnScroll
-      className="flex w-full justify-between py-3 items-center"
-    >
-      <NavbarBrand>
-        <p className="font-bold text-inherit text-lg">CafeZiq5</p>
-      </NavbarBrand>
+    <div className="w-full flex justify-center pt-5">
+      <Navbar
+        shouldHideOnScroll
+        className="flex  justify-between py-3 items-center text-[#1e2b23] shadow-[0_0_15px_3px_rgba(0,0,0,0.1)] bg-[#ffffffba] px-8 rounded-xl w-[85%]"
+      >
+        <NavbarBrand className="w-[25%]">
+          <p className="font-bold text-lg text-[#3a5c47]">CafeZiq5</p>
+        </NavbarBrand>
 
-      <NavbarContent className="hidden sm:flex gap-16 group" justify="center">
-        <NavbarItem className="group-hover:opacity-50 hover:!opacity-100 transition-opacity duration-200">
-          <Link color="foreground" href="#">
-            About Us
-          </Link>
-        </NavbarItem>
-        <NavbarItem className="group-hover:opacity-50 hover:!opacity-100 transition-opacity duration-200">
-          <Link aria-current="page" href="#">
-            Feedback
-          </Link>
-        </NavbarItem>
-        <NavbarItem className="group-hover:opacity-50 hover:!opacity-100 transition-opacity duration-200">
-          <Link color="foreground" href="#">
-            Menu
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
+        <NavbarContent className="hidden sm:flex gap-16 group" justify="center">
+          <NavbarItem className="group-hover:opacity-50 hover:!opacity-100 transition-opacity duration-200">
+            <Link color="foreground" href="#">
+              About Us
+            </Link>
+          </NavbarItem>
+          <NavbarItem className="group-hover:opacity-50 hover:!opacity-100 transition-opacity duration-200">
+            <Link aria-current="page" href="#">
+              Feedback
+            </Link>
+          </NavbarItem>
+          <NavbarItem className="group-hover:opacity-50 hover:!opacity-100 transition-opacity duration-200">
+            <Link color="foreground" href="#">
+              Menu
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
 
-      <NavbarContent justify="end" className="gap-16">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Button
-            className="bg-orange-500 px-4 py-1.5 cursor-pointer text-white rounded-lg"
-            variant="shadow"
-          >
-            Book
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
-    </Navbar>
+        <NavbarContent
+          justify="end"
+          className="gap-10 w-[25%] flex justify-end"
+        >
+          <NavbarItem className="hidden lg:flex">
+            <Link
+              href="#"
+              className="border-[2px] border-[#dadada63] px-4 py-1.5 rounded-full"
+            >
+              Login
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Button
+              className="bg-[#3b5645] px-4 py-1.5 cursor-pointer text-white rounded-full"
+              variant="shadow"
+            >
+              Book Now
+            </Button>
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
+    </div>
   );
 }
--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -3,11 +3,41 @@
 import React from "react";
 import { Button } from "@heroui/react";
 import MyNavbar from "./Component/Navbar";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <main className="flex min-h-screen flex-col px-24">
+    <main className="flex min-h-screen flex-col px-24 bg-[#EDF4F2]">
       <MyNavbar />
+
+      <div className="flex flex-col w-full pt-20 items-center gap-3 h-[85vh] text-[#1e2b23]">
+        <Link
+          href={"/"}
+          className="bg-white px-3 py-1.5 shadow-[0_0_15px_3px_rgba(0,0,0,0.1)] rounded-lg mb-5"
+        >
+          Welcome to CafeZiq5
+        </Link>
+        <p className="text-6xl font-semibold text-center leading-[70px]">
+          Your Space, Your Time <br /> Book a Cabin Now!
+        </p>
+        <p className="w-[37%] text-base text-[#000000c9] text-center">
+          Get a quiet space for work, meetings, or just relaxation? Secure your
+          private cabin in a few clicks
+        </p>
+        <p className="text-[#31473A] flex gap-3">
+          Instant Online Booking <span className="text-black">|</span> ₹200/hour
+          <span className="text-black"> | </span> Two Branches Available
+        </p>
+        <Button
+          className="bg-[#2f4537] text-white custom-button rounded-xl py-2 px-4 mt-10 cursor-pointer"
+          onClick={() => router.push("/book")}
+        >
+          Book Now
+        </Button>
+      </div>
     </main>
   );
 }
