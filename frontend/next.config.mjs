9111826eb838cd5e316f564556c4720d985a5bd8
Aayush Chaudhary <aayushcyy@gmail.com>
2025-03-24 17:41:21 +0530

    again changed config js file

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,8 +3,6 @@ const nextConfig = {
   images: {
     domains: ["www.shutterstock.com"],
   },
-  output: "export", // Ensures static export is properly handled
-  trailingSlash: true,
 };
 
 export default nextConfig;
